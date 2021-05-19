262e44708a5b14b9bc647efb95e233b273e3b6ad
Bziomek97 <B.ziomek97@gmail.com>
2021-05-19 11:31:39 +0200

    Web scrapping as a initial commit

---
+++ tools/logger.py
@@ -0,0 +1,24 @@
+import time
+import datetime
+
+class Logger:
+    debug_mode = False
+
+    def __init__(self, componentName: str) -> None:
+        self.component = componentName
+
+    def log_entry(self, message: str, level: str = 'inf') -> None:
+        '''Function to create and print logs
+        
+        Parameters: \n
+        message - string with message \n
+        level - info about type of log ['inf', 'warn', 'err', 'debug']. Default inf
+
+        '''
+        if not level in ['inf', 'warn', 'err', 'debug']:
+            raise TypeError("Log's level doesnt fit to type. Instead of use one of this: ['inf', 'warn', 'err', 'debug']")
+        elif self.debug_mode and level == 'debug':
+            return
+
+        st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        print("[%s][%s][%s] %s" % (st, self.component, level, message))
